010d2187f6ca5fb6a6bcb113c1ea347e4dbf0e81
Joao-lucas-felix <jlmacena06@gmail.com>
2026-02-02 12:20:31 -0300

    Reset the project

--- cap-1-tickets/db/migrate/20250106164011_add_phone_to_tickets.rb
+++
@@ -1,5 +0,0 @@
-class AddPhoneToTickets < ActiveRecord::Migration[7.1]
-  def change
-    add_column :tickets, :phone, :string
-  end
-end
--- cap-2-MeBay/app/controllers/ads_controller.rb
+++
@@ -1,9 +0,0 @@
-class AdsController < ApplicationController
-  def show
-    @ad = Ad.find(params[:id])
-  end
-
-  def index
-    @ads = Ad.all
-  end
-end
